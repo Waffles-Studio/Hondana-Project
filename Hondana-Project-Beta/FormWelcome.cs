ad2aedc856268f143352b8e3429d24302cd22b19
Brian Ba3 <63374540+BrianBa3@users.noreply.github.com>
2021-12-08 13:48:06 -0600

    First screen code

--- Hondana-Project-Beta/FormWelcome.cs
+++ Hondana-Project-Beta/FormWelcome.cs
@@ -8,19 +8,91 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.IO;
+using System.Data.SqlClient;
+
 namespace Hondana_Project_Beta
 {
     public partial class FormWelcome : Form
     {
+
+        #region Variables
+        Globales Glb = new Globales();
+
+        #endregion
+
+        #region Inicio
         public FormWelcome()
         {
             InitializeComponent();
+            add();
         }
+        #endregion
 
+        #region Login
         private void BtnLogin_Click(object sender, EventArgs e)
         {
-            FormLoginSignup FLS = new FormLoginSignup();
-            FLS.ShowDialog();
+            Glb.conexion = new SqlConnection("Data Source=" + cmbserv.SelectedItem + ";Initial Catalog=HondanaDB;Integrated Security=True");
+            try
+            {
+                Glb.conexion.Open();
+                Glb.conexion.Close();
+                FormLoginSignup FLS = new FormLoginSignup();
+                FLS.ShowDialog();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Please connect to your server first", ":(");
+            }
+
+
+
         }
+        #endregion
+
+        #region conectarse
+        private void BtnConnect_Click(object sender, EventArgs e)
+        {
+            Glb.conexion = new SqlConnection("Data Source=" + cmbserv.SelectedItem + ";Initial Catalog=HondanaDB;Integrated Security=True");
+
+            try
+            {
+                Glb.conexion.Open();
+                MessageBox.Show("Established connection", ":)");
+                Glb.conexion.Close();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Failed to connect. \nCheck your server name", ":(");
+                Glb.conexion.Close();
+            }
+        }
+        #endregion
+
+        #region AgregarServers
+        private void txtaddserver_Click(object sender, EventArgs e)
+        {
+            StreamWriter agregar = File.AppendText("ConDB.txt");
+            agregar.WriteLine(cmbserv.Text);
+            agregar.Close();
+
+            add();
+        }
+        #endregion
+
+        #region combobox
+        private void add()
+        {
+            cmbserv.Items.Clear();
+            StreamReader con = new StreamReader("ConDB.txt");
+            string s = con.ReadLine();
+            while (s != null)
+            {
+                cmbserv.Items.Add(s);
+                s = con.ReadLine();
+            }
+            con.Close();
+        }
+        #endregion
     }
 }
